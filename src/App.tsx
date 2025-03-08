9d53254537eaa521ada7d6472226823d782dca21
Gabriel Rocha Stimamiglio <gabrielstimamiglio@gmail.com>
2025-03-08 19:23:55 -0300

    Finalizing the Footer and developing the Navbar's responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./assets/css/index.css";
+import appStyle from "./assets/css/App.module.css";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 
@@ -6,7 +7,17 @@ export const App = () => {
   return (
     <>
       <Navbar />
-      <main>Mais</main>
+      <main>
+        <section className={appStyle.sectionOne}>
+          <h1>
+            Noona Digital Cashier <br /> Monitor Outlet Revenue Receipts
+          </h1>
+          <p>
+            we developed an innovative and efficient point-of-sale (POS)
+            application specifically designed for the Indonesian MSME market.
+          </p>
+        </section>
+      </main>
       <Footer />
     </>
   );
